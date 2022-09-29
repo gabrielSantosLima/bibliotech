b49355d5a2d77901cb12b4badc20e7ecbe235338
FMABr <felipemuamorim@gmail.com>
2022-09-28 23:11:15 -0400

    CORS Accept header

--- src/app.js
+++ src/app.js
@@ -5,12 +5,12 @@ import routes from "./routes/index.js"
 
 db.on("error", console.log.bind(console, 'Erro de conexão'));
 db.once("open", () => {
-    console.log('Conexão feita com suceso com o DB')
+    console.log('Conexão feita com sucesso com o DB')
 });
 
 const app = express();
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:5500'
 }))
 app.use(express.json());
 routes(app);
