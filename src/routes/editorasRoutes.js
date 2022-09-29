9587b7bd86a2f53b2fc3b7417b2d823499c5917f
FMABr <felipemuamorim@gmail.com>
2022-09-28 22:58:25 -0400

    EditoraController Routes

--- src/routes/editorasRoutes.js
+++ src/routes/editorasRoutes.js
@@ -4,5 +4,9 @@ import EditoraController from "../controllers/editoraController.js"
 const router = express.Router()
 
 router.get("/editoras", EditoraController.listarEditoras)
+      .get("/editoras/:id", EditoraController.listarEditoraPorId)
+      .post("/editoras", EditoraController.cadastrarEditora)
+      .put("/editoras/:id", EditoraController.atualizarEditora)
+      .delete("/editoras/:id", EditoraController.excluirEditora)
 
-export default router;
+export default router;  
